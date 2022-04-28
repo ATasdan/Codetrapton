ae213d06254b4dee9287157cad8c880ecc0c1223
Anıl Taşdan <anil.tasda@gmail.com>
2022-04-28 06:20:40 +0300

    db test

--- db/config.php
+++ db/config.php
@@ -1,5 +1,5 @@
 <?php
-    $connectionString = "postgres://mrutglhlxpxpwi:2e079d4b99acd29378091e7b9471a1ef3040539db5c615d942f5774b2b556017@ec2-34-194-158-176.compute-1.amazonaws.com:5432/deacap1jo5th24";
+    $connectionString = "ec2-34-194-158-176.compute-1.amazonaws.com";
     $port=5432;
     $username="mrutglhlxpxpwi";
     $password="2e079d4b99acd29378091e7b9471a1ef3040539db5c615d942f5774b2b556017";
