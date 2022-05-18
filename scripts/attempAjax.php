76c23ceaccab8699fc12ebb9ec4b7c1918369045
boraaltinok <63509903+boraaltinok@users.noreply.github.com>
2022-05-18 09:00:47 +0300

    bora3

--- scripts/attempAjax.php
+++ scripts/attempAjax.php
@@ -14,11 +14,13 @@
 
         //echo "result = $result";
     }
+
     $query = "SELECT solution FROM question WHERE question_id = $specific_question_id";
     $result = pg_query($db_conn, $query);
     $row = pg_fetch_assoc($result);
+    $trimmedSolution = preg_replace('/\s+/', '', "".$row['solution']."");
 
-    if(strcasecmp("".$row['solution']."", $trimmedString ) == 0){
+    if(strcasecmp(trim($trimmedSolution), $trimmedString ) == 0){
         echo json_encode("TRUE");
         $query = "UPDATE user_question_attempt SET iscorrect = TRUE WHERE user_id = $user_id AND attempt_id = $newAttemptId AND question_id = $specific_question_id";
         $result = pg_query($db_conn, $query);  
